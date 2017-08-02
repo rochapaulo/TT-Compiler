cd9f2262a6676e5a1b716ba1aa6ccda8e4054f32
Paulo Almeida <almeida.paulorocha@gmail.com>
2017-08-02 10:51:13 -0300

    started intermediate code generation

--- ir/IRTreeGen.cpp
+++ ir/IRTreeGen.cpp
@@ -9,57 +9,75 @@ IRTreeGen::IRTreeGen() {
 }
 
 IRNode IRTreeGen::genCode(AST_Program *program) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NBinaryOperation *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NNegation *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NReturn *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NIdentifier *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NAssign *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NIf *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NWhile *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NFor *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NBreak *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NArrayCreation *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NArray *exp){
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NArrayAssign *exp){
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NFunctionCall *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NExpressionList *exp){
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NInteger *exp) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NFunctionDec *dec) {
+    // TODO
 }
 
 IRNode IRTreeGen::genCode(NImport *dec) {
+    // TODO
 }
 
 vector<IRNode*> IRTreeGen::getIntermediateCode(AST_Program *program) {
